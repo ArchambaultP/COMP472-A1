d41ef28fa6f9b3efb27c4f2757cc1ef8ce15ea85
pascal archambault <pascalarchambault@pop-os.localdomain>
2020-10-09 17:37:39 -0400

    formatted training data

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-import sklearn as sk
 import csv
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 def main():
     path = Path('./dataset')
 
     symbol_dict = {}
     samples = {}
+    training_data = []
 
     with open(path / 'info_1.csv') as file:
         reader = csv.reader(file, delimiter='\n') 
@@ -20,17 +21,26 @@ def main():
             except Exception as e:
                 continue
     
-    print(symbol_dict)
+    # print(symbol_dict)
 
     with open(path / 'train_1.csv') as file:
         reader = csv.reader(file, delimiter='\n') 
         for row in reader:
-            index = int(row[0].split(',')[-1])
+            split_row = row[0].split(',')
+            index = int(split_row[-1])
             samples[index] += 1
+            training_data.append([int(string) for string in split_row[:-1]])
 
-    print(samples)
-    
- 
+
+    # print(samples)
+
+    symbs = symbol_dict.values()
+    vals = samples.values()
+    fig, ax = plt.subplots()
+    ax.bar(symbs, vals)
+
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
