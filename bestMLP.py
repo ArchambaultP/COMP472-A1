907479b409c3053ec5aa6cac7f829e5a3de46b93
IzzoJer <izzo.j@hotmail.com>
2020-10-13 15:32:53 -0400

    not 100% accuracy with best model combination, i can still play around with the hidden layers

--- bestMLP.py
+++ bestMLP.py
@@ -1,31 +1,49 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
-
-def bestMLP(X_train, Y_train, X_val, Y_val, X_test, Y_test):
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+import pandas as pd
+def bestMLP(X_train, Y_train, X_val, Y_val, X_test):
 	
 
+	def model(hlsizes, a, s):
+		mlp = MLPClassifier(hidden_layer_sizes = hlsizes, activation = a, solver = s)
+		mlp.fit(X_train, Y_train)
+		return mlp		
 
-
-	def model():
-		
-		parameters = {
-    				'hidden_layer_sizes': [(30,50), (10,10,10)],
-    				'activation': ['tanh', 'relu', 'sigmoid', 'identity'],
-    				'solver': ['sgd', 'adam'],
-    				}
-   
-		mlp = MLPClassifier()
-		gsMLP = GridSearchCV(mlp, parameters)
-		gsMLP.fit(X_train, Y_train)
-
-		print(gsMLP.predict(X_val))
-		
-
-	def predictions(model, X_test, Y_test):
-		
-		pred = model.predict(X_test)
+	def predictions(model, X):
+		pred = model.predict(X)
 		return pred
 
-	model()
+	def stats(pred):
+		f1Micro = f1_score(Y_val, pred, average='micro')
+		f1Macro = f1_score(Y_val, pred, average='macro')
+		accuracy = accuracy_score(Y_val, pred)
+		return (f1Micro + f1Macro + accuracy) / 3
+
+	def bestParams():
+		
+		best = 0
+		bestparams = [] 
+
+		parameters = {
+    			'hidden_layer_sizes': [(30,50,), (10,10,10,)],
+    			'activation': ['tanh', 'relu', 'logistic', 'identity'],
+    			'solver': ['sgd', 'adam'],
+    			}
+		for x in parameters['hidden_layer_sizes']:
+			for y in parameters['activation']:
+				for z in parameters['solver']:
+					pred = predictions(model(x,y,z), X_val)
+					print(x,y,z)
+					statss = stats(pred)
+					print(statss)
+					if statss > best:
+						best = statss
+						bestparams = [x,y,z]
+		return bestparams
 
 
+	best = bestParams()
+
+	print(predictions(model(best[0], best[1], best[2]), X_test))
